cb65acccb982b229098ce7cf7bcc6810dc38cedd
Birillo Ilya <vipilia@mail.ru>
2022-04-10 19:46:11 +0300

    featur: copleted task (CounterWithRedux) and coppied appi to redux

--- home_react/src/App.tsx
+++ home_react/src/App.tsx
@@ -1,11 +1,13 @@
 import  React, {useState} from "react";
-import {createGlobalStyle, ThemeProvider} from 'styled-components';
+import {createGlobalStyle, ThemeProvider} from 'styled-components'; 
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 import { themes } from "./components/styledComponents/themes";
 import  {Button} from "./components/Button/Button";
 import ButtonGroup from "./components/ButtonGroup/ButtonGroup";
 import {ButtonSt} from "./components/styledComponents/Button/ButtonSt";
 import {Context} from "./components/Context/Context"
-import {News} from "./api/containers/news-container"
+// import {News} from "./api/containers/news-container"
 import {Icon} from "./components/Icon/Icon"
 import {Img} from "./components/Img/Img"
 import Input from "./components/Input/Input"
@@ -14,8 +16,11 @@ import {CheckBox} from "./components/CheckBox/CheckBox"
 import { ButtonStyle } from "./components/styledComponents/Button/ButtonSt";
 import {Select} from "./components/Select/Select"
 import {SelectCustom} from "./components/Select/Select"
-import {Counter} from "./components/Counter/Counter"
-// import Countdown from "./components/Counter/Counter"
+// import {Counter} from "./components/Counter/Counter"
+import CounterWithReduxClasses from "./components/CounterWithRedux/CounterWithReduxClasses"; 
+import { CounterWithReduxFunctional } from "./components/CounterWithRedux/CounterWithReduxFunctional"; 
+import NewsPage from "./useState-useEffect-useCallback/pages/NewsPage";
+
 
 function App() {
   
@@ -25,7 +30,7 @@ function App() {
   // }
 
   return (
-    <>
+  <>
     <ThemeProvider theme = {themes}>
     {/* <ButtonGroup vertical className="wrapperButtonGroup">
       <Button className="btn"/>
@@ -37,8 +42,6 @@ function App() {
     <ButtonSt className="la" children = "Button SC"/>
 
     <Context/>
-    
-    <News/> */}
 
     {/* <ButtonStyle>Button</ButtonStyle> */}
 
@@ -47,11 +50,11 @@ function App() {
 
       {/* <Icon name = "cat" size={65}></Icon>
 
-<Img alt = "jopa" circle/> */}
+    <Img alt = "jopa" circle/> */}
 
     {/* <Input setErrors={setErrors} className="inputText" label = "text" id = "inputId" 
     errors={errors}
-  onInputValidation={onInputValidation}></Input>  */}
+    onInputValidation={onInputValidation}></Input>  */}
 
     
     {/* <Form ></Form>  */}
@@ -63,12 +66,22 @@ function App() {
 
     {/* <SelectCustom></SelectCustom> */}
 
-  <Counter></Counter>
+    {/* <Counter></Counter> */}
+
+
+    {/* ==================================REDUX=============================================== */}
+
+    <Provider store={store}>
+      <CounterWithReduxClasses></CounterWithReduxClasses>
+      <CounterWithReduxFunctional></CounterWithReduxFunctional>
+
+      {/* <NewsPage></NewsPage> */}
+    </Provider>
 
 
 
-  </ThemeProvider> 
-    </>
+    </ThemeProvider> 
+  </>
   );
 }
 
